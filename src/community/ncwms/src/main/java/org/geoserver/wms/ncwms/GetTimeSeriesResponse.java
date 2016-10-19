fd7fc8df9faa6bd040d9828f93129967c6137ac1
dispiste <cesar.izq@gmail.com>
2016-10-19 19:37:57 +0200

    Add JFreechart output to GetTimeSeries

--- src/community/ncwms/src/main/java/org/geoserver/wms/ncwms/GetTimeSeriesResponse.java
+++ src/community/ncwms/src/main/java/org/geoserver/wms/ncwms/GetTimeSeriesResponse.java
@@ -10,13 +10,13 @@ import java.io.PrintWriter;
 import java.nio.charset.Charset;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.geoserver.ows.Response;
 import org.geoserver.ows.util.OwsUtils;
@@ -27,29 +27,35 @@ import org.geoserver.wms.WMS;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.FeatureIterator;
 import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.util.logging.Logging;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.time.Millisecond;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
 import org.opengis.feature.Feature;
 import org.opengis.feature.simple.SimpleFeature;
-import org.opengis.feature.simple.SimpleFeatureType;
-import org.opengis.feature.type.AttributeDescriptor;
-import org.opengis.feature.type.Name;
 import org.springframework.util.Assert;
 
 import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
 
 import net.opengis.wfs.FeatureCollectionType;
 
 public class GetTimeSeriesResponse extends Response{
+    private static final Logger LOGGER = Logging.getLogger(GetTimeSeriesResponse.class);
     protected static final Set<String> outputFormats = new HashSet<String>();
     static {
         outputFormats.add("text/csv");
         outputFormats.add("image/png");
         outputFormats.add("image/jpg");
-        outputFormats.add("image/jpeg");        
+        outputFormats.add("image/jpeg");
     }
     
     private WMS wms;
     private final static String ISO8601_2000_UTC_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    private final static int IMAGE_HEIGHT = 600, IMAGE_WIDTH = 700;
     
     public GetTimeSeriesResponse(final WMS wms) {
         super(FeatureCollectionType.class, outputFormats);
@@ -87,8 +93,72 @@ public class GetTimeSeriesResponse extends Response{
         GetFeatureInfoRequest request = (GetFeatureInfoRequest) operation.getParameters()[0];
         FeatureCollectionType results = (FeatureCollectionType) value;
 
-        //TODO: implement JFreeChart output formats too
-        writeCsv(request, results, output);
+        String mime = getMimeType(value, operation);
+        if (mime.startsWith("image/")) {
+            writeChart(request, results, output, mime);
+        }
+        else {
+            writeCsv(request, results, output);
+        }
+    }
+    
+    @SuppressWarnings("rawtypes")
+    private void writeChart(GetFeatureInfoRequest request, FeatureCollectionType results, OutputStream output, String mimeType) {
+        final TimeSeries series = new TimeSeries("time", Millisecond.class);
+        String valueAxisLabel = "Value";
+        
+        Charset charSet = wms.getCharSet();
+        OutputStreamWriter osw = new OutputStreamWriter(output, charSet);
+        
+        FeatureIterator reader = null;
+        try {
+            final List collections = results.getFeature();
+            if (collections.size()==1) { // we only consider the result if a single layer was queried
+                FeatureCollection fr;
+                SimpleFeature f;
+                fr = (FeatureCollection) collections.get(0);
+                valueAxisLabel = fr.getSchema().getName().getLocalPart();
+                
+                reader = fr.features();
+                while (reader.hasNext()) {
+                    Feature feature = reader.next();
+                    if (feature instanceof SimpleFeature)
+                    {
+                        f = (SimpleFeature) feature;
+                        Date date = (Date) f.getAttribute("date");
+                        Double value  = (Double) f.getAttribute("value");
+                        
+                        series.add(new Millisecond(date), value);
+                    }
+                }
+            }
+        } catch (Exception ife) {
+            LOGGER.log(Level.WARNING, "Error generating getFeaturInfo, HTML format", ife);
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+        XYDataset dataset =  new TimeSeriesCollection(series);
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(             
+                "Time series of M", 
+                "Date / time",
+                valueAxisLabel,
+                dataset,
+                false,
+                false,
+                false);
+        try {
+            if (mimeType.startsWith("image/png")) {
+                ChartUtilities.writeChartAsPNG(output, chart, IMAGE_WIDTH, IMAGE_HEIGHT);
+            }
+            else if (mimeType.equals("image/jpg") || mimeType.equals("image/jpeg")) {
+                ChartUtilities.writeChartAsJPEG(output, chart, IMAGE_WIDTH, IMAGE_HEIGHT);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
     
     @SuppressWarnings("rawtypes")
@@ -112,8 +182,9 @@ public class GetTimeSeriesResponse extends Response{
                 FeatureCollection fr;
                 SimpleFeature f;
                 fr = (FeatureCollection) collections.get(0);
-                reader = fr.features();
                 writer.println("Time (UTC),"+fr.getSchema().getName().getLocalPart());
+                
+                reader = fr.features();
                 while (reader.hasNext()) {
                     Feature feature = reader.next();
                     if (feature instanceof SimpleFeature)
@@ -126,16 +197,14 @@ public class GetTimeSeriesResponse extends Response{
                 }
             }
         } catch (Exception ife) {
-            //LOGGER.log(Level.WARNING, "Error generating getFeaturInfo, HTML format", ife);
+            LOGGER.log(Level.WARNING, "Error generating getFeaturInfo, HTML format", ife);
             writer.println("Unable to generate information " + ife);
         } finally {
             if (reader != null) {
                 reader.close();
             }
         }
-
         writer.flush();
-
     }
     
     private String formatAsISO8601_2000_UTC(Date date) {
